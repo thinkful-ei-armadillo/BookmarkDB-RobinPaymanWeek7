5dd6662924253e2beccc2db983c32da3fdc8fa61
Robin Khiv <robinkhiv@gmail.com>
2019-02-27 12:20:44 -0800

    seed files added

---
+++ test/bookmarks.fixtures.js
@@ -0,0 +1,18 @@
+function makeBookmarkArray (){
+  return [{
+    id: 1,
+    title: 'hey',
+    url: 'https://google.com',
+    rating: 1,
+    description: 'wuddup playa'
+  },
+  {
+    id: 2,
+    title: 'yo wuddup b',
+    url: 'https://thinkful.com',
+    rating: 3,
+    description: 'yo jus chillin'
+  }
+  ];
+}
+module.exports = {makeBookmarkArray};
